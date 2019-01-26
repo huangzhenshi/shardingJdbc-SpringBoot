4cc8ac6b1b9177eec58ed65c63b1666e4436d065
huangzhenshi <570247284@qq.com>
2019-01-26 17:08:04 +0800

    11

---
+++ fangjia-sjdbc-sharding-table-springboot/src/main/java/com/fangjia/sharding/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.fangjia.sharding.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fangjia.sharding.po.User;
+import com.fangjia.sharding.service.UserService;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/users")
+	public Object list() {
+		return userService.list();
+	}
+	
+	@GetMapping("/add")
+	public Object add() {
+		for (long i = 0; i < 100; i++) {
+			User user = new User();
+			user.setId(i);
+			user.setCity("深圳"+i);
+			user.setName("李四male");
+			user.setSex(0);
+			userService.add(user);
+		}
+		
+		for (long i = 0; i < 100; i++) {
+			User user = new User();
+			user.setId(i);
+			user.setCity("苏州");
+			user.setName("小燕儿female");
+			user.setSex(1);
+			userService.add(user);
+		}
+		return "success";
+	}
+	
+	@GetMapping("/users/{id}")
+	public Object get(@PathVariable Long id) {
+		return userService.findById(id);
+	}
+	
+	@GetMapping("/users/query")
+	public Object get(String name) {
+		return userService.findByName(name);
+	}
+	
+}
---
+++ fangjia-sjdbc-sharding-table-springboot/src/main/java/com/fangjia/sharding/po/User.java
@@ -0,0 +1,50 @@
+package com.fangjia.sharding.po;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -1205226416664488559L;
+	
+	private Long id;
+
+	private String city = "";
+	
+	private String name = "";
+	
+	private Integer sex;
+
+	public Integer getSex() {
+		return sex;
+	}
+
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
